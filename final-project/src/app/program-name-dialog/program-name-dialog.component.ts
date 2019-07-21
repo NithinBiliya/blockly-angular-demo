1d6cd0af77a06780956cf08d624a2f1e848f72c8
Nithin Biliya <nithin.biliya@yahoo.in>
2019-07-21 18:16:53 +0530

    fix prod build issue

--- final-project/src/app/program-name-dialog/program-name-dialog.component.ts
+++ final-project/src/app/program-name-dialog/program-name-dialog.component.ts
@@ -9,7 +9,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class ProgramNameDialogComponent implements OnInit {
   constructor(
     public dialogRef: MatDialogRef<ProgramNameDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: string
+    @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
 
   ngOnInit() {}
